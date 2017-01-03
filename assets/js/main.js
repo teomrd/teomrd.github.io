ea87c161cb193d26864bb8b0aff2aa630d70acbe
Mironidis Theodoros <teomrd@hotmail.com>
2017-01-03 22:09:53 +0000

    Printing util printing but not complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,21 @@
 
 $(document).ready(() => {
   kendo.pdf.defineFont({
-    'Open Sans': 'plugins/fonts/fonts-open-sans/OpenSans-Regular.ttf',
+    // 'Josefin Sans': 'http://mironidis.com/dojo/fonts/Josefin_Sans/JosefinSans-Regular.ttf',
+    // 'Josefin Sans|Bold': 'fonts/Josefin_Sans/JosefinSans-Bold.ttf',
     'FontAwesome': 'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/fonts/fontawesome-webfont.ttf',
   });
 
   $('.print').click(() => {
+    $('body').addClass('size-a4');
     kendo.drawing.drawDOM($('body'))
     .then((group) => kendo.drawing.exportPDF(group, {
       paperSize: 'auto',
-      margin: { left: '1cm', top: '1cm', right: '1cm', bottom: '1cm' },
+      margin: { top: 0, left: 0, right: 0, bottom: 0 },
+      multiPage: true,
     }))
     .done((data) => {
+      $('body').removeClass('size-a4');
       kendo.saveAs({
         dataURI: data,
         fileName: 'MironidisTheodorosCV.pdf',
