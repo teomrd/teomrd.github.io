ba4fa882cf0c697b44cc0dc70ce2dd88cf07b3d5
Mironidis Theodoros <teomrd@hotmail.com>
2017-01-06 10:24:33 +0000

    Print util almost ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,32 @@
 
 $(document).ready(() => {
   kendo.pdf.defineFont({
-    'Josefin Sans': 'assets/fonts/Josefin_Sans/JosefinSans-Regular.ttf',
-    'Josefin Sans|Bold': 'assets/fonts/Josefin_Sans/JosefinSans-Bold.ttf',
-    'FontAwesome': 'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/fonts/fontawesome-webfont.ttf',
+    'Josefin Sans': '/assets/fonts/Josefin_Sans/JosefinSans-Regular.ttf',
+    'Josefin Sans|Bold': '/assets/fonts/Josefin_Sans/JosefinSans-Bold.ttf',
+    'Josefin Sans|Italic': '/assets/fonts/Josefin_Sans/JosefinSans-Italic.ttf',
+    'Josefin Sans|Bold|Italic': '/assets/fonts/Josefin_Sans/JosefinSans-BoldItalic.ttf',
+    'FontAwesome': 'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf',
   });
 
   $('.print').click(() => {
     $('body').addClass('size-a4');
     kendo.drawing.drawDOM($('body'))
     .then((group) => kendo.drawing.exportPDF(group, {
+      title: 'Mironidis Theodoros CV',
+      author: 'Mironidis Theodoros',
+      keywords: 'teomrd, Mironidis, mironidis.gr',
+      creator: 'teomrd',
       paperSize: 'auto',
       margin: { top: 0, left: 0, right: 0, bottom: 0 },
-      multiPage: true,
     }))
     .done((data) => {
       $('body').removeClass('size-a4');
       kendo.saveAs({
         dataURI: data,
         fileName: 'MironidisTheodorosCV.pdf',
-        proxyURL: '//demos.telerik.com/kendo-ui/service/export',
+        function() {
+          console.log('komple');
+        },
       });
     });
   });
