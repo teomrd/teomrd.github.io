16924b23c864ed168ceef28c91f2a99b98a726a1
teomrd <mironidis.gr@gmail.com>
2018-08-20 23:49:42 +0100

    Simplified printing, made it available in prod and remove extraneous deps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,3 @@
-/* global $, document, kendo */
-
-$(document).ready(() => {
-  kendo.pdf.defineFont({
-    'Josefin Sans': '/assets/fonts/Josefin_Sans/JosefinSans-Regular.ttf',
-    'Josefin Sans|Bold': '/assets/fonts/Josefin_Sans/JosefinSans-Bold.ttf',
-    'Josefin Sans|Italic': '/assets/fonts/Josefin_Sans/JosefinSans-Italic.ttf',
-    'Josefin Sans|Bold|Italic': '/assets/fonts/Josefin_Sans/JosefinSans-BoldItalic.ttf',
-    'Material Icons': '/assets/fonts/Material_Icons/MaterialIcons-Regular.ttf',
-  });
-
-  $('.print').click(() => {
-    $('body').addClass('size-a4');
-    kendo.drawing.drawDOM($('body'))
-    .then((group) => kendo.drawing.exportPDF(group, {
-      title: 'Mironidis Theodoros CV',
-      author: 'Mironidis Theodoros',
-      keywords: 'teomrd, Mironidis, mironidis.gr',
-      creator: 'teomrd',
-      paperSize: 'auto',
-      margin: { top: 0, left: 0, right: 0, bottom: 0 },
-    }))
-    .done((data) => {
-      $('body').removeClass('size-a4');
-      kendo.saveAs({
-        dataURI: data,
-        fileName: `MironidisTheodorosCV_${kendo.toString(new Date(), 'dd MMM yyyy')}.pdf`,
-      });
-    });
-  });
-});
+(function() {
+  document.getElementById("print").onclick = () => window.print();
+})();
