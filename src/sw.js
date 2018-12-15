570b02c8048a13010b34afb38ae1d51b6fad6ac9
teomrd <mironidis.gr@gmail.com>
2018-12-15 16:24:46 +0000

    Progressive Web App ✔️

--- src/sw.js
+++ src/sw.js
@@ -39,3 +39,30 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      console.log(
+        "[Service Worker] Network request Failed. Serving content from cache: " +
+          error
+      );
+      //Check to see if you have it in the cache
+      //Return response
+      //If not in the cache, then return error page
+      return caches
+        .open(
+          "sw-precache-v3-sw-precache-webpack-plugin-https://silent-things.surge.sh"
+        )
+        .then(function(cache) {
+          return cache.match(event.request).then(function(matching) {
+            var report =
+              !matching || matching.status == 404
+                ? Promise.reject("no-match")
+                : matching;
+            return report;
+          });
+        });
+    })
+  );
+});
