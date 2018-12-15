e4e7c069d7a6b7dde9ff0e3c77593e56d82af93e
teomrd <mironidis.gr@gmail.com>
2018-12-15 18:53:14 +0000

    fixed sw registration

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,3 @@
 (function() {
   document.getElementById("print").onclick = () => window.print();
-
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("../../sw.js");
-    });
-  }
 })();
